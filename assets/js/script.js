b13f820026297ad2ec5c9262385a17601bf8d4ad
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-01-23 00:55:35 -0600

    dropping items in the delete zone removes item

--- assets/js/script.js
+++ assets/js/script.js
@@ -204,3 +204,18 @@ $(".card .list-group").sortable({
     saveTasks();
   }
 });
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  },
+})
