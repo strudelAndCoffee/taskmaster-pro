a1050e14556354cbd1a21f36d75f03eb825bf32b
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-01-24 13:03:58 -0600

    added 30 minute interval to audit tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -247,3 +247,9 @@ $("#trash").droppable({
 $("#modalDueDate").datepicker({
   minDate: 1
 });
+
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, 1800000);
